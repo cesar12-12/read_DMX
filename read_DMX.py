6546833cb10d0cfc1485791b35189181f31ea6b9
cesar12-12 <jlcesar_12@outlook.es>
2022-06-03 10:42:47 -0500

    First Commit

---
+++ read_DMX.py
@@ -0,0 +1,25 @@
+import sys
+import os 
+from tkinter import N
+
+
+#python3 prog.py /path Cambio NewWord New_dir_name
+
+Archivos=sys.argv #Lista con los argumentos (archivos)
+
+path=os.listdir(Archivos[1])
+Cambio_palabra=Archivos[2]
+Nueva_palabra=Archivos[3]
+Dir_name=os.mkdir(Archivos[4])
+
+for Archivo in path: #para cada dentro del directorio 
+    Archivo_original=open(Archivos[1]+"\\"+Archivo,'r')
+    Archivo_Nuevo=open(Archivos[4]+"\\"+Archivo.replace(Cambio_palabra, Nueva_palabra),'w')
+    for linea in Archivo_original: #para cada linea de Archivo
+        if Cambio_palabra in linea: #si esta la palabra que se desea cambiar dentro de la variable "linea"
+            nueva_linea=linea.replace(Cambio_palabra, Nueva_palabra)
+            Archivo_Nuevo.write(nueva_linea)
+        else:
+            Archivo_Nuevo.write(linea)
+    Archivo_Nuevo.close()
+    Archivo_original.close()
