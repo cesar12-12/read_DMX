31b66a407135a179c6c10cca40c1b8eeef6ae613
cesar12-12 <jlcesar_12@outlook.es>
2022-06-09 10:57:26 -0500

    Difference in dir and file added

--- read_DMX.py
+++ read_DMX.py
@@ -10,16 +10,21 @@ Archivos=sys.argv #Lista con los argumentos (archivos)
 path=os.listdir(Archivos[1])
 Cambio_palabra=Archivos[2]
 Nueva_palabra=Archivos[3]
-Dir_name=os.mkdir(Archivos[4])
 
-for Archivo in path: #para cada dentro del directorio 
-    Archivo_original=open(Archivos[1]+"\\"+Archivo,'r')
-    Archivo_Nuevo=open(Archivos[4]+"\\"+Archivo.replace(Cambio_palabra, Nueva_palabra),'w')
-    for linea in Archivo_original: #para cada linea de Archivo
-        if Cambio_palabra in linea: #si esta la palabra que se desea cambiar dentro de la variable "linea"
-            nueva_linea=linea.replace(Cambio_palabra, Nueva_palabra)
-            Archivo_Nuevo.write(nueva_linea)
-        else:
-            Archivo_Nuevo.write(linea)
-    Archivo_Nuevo.close()
-    Archivo_original.close()
+if os.path.exists(Archivos[4]) is False:
+    Dir_name=os.mkdir(Archivos[4])
+else:
+    pass
+
+for Archivo in path: #para cada dentro del directorio
+    if os.path.isfile(Archivos[1]+"\\"+Archivo) is True:
+        Archivo_original=open(Archivos[1]+"\\"+Archivo,'r')
+        Archivo_Nuevo=open(Archivos[4]+"\\"+Archivo.replace(Cambio_palabra, Nueva_palabra),'w')
+        for linea in Archivo_original: #para cada linea de Archivo
+            if Cambio_palabra in linea: #si esta la palabra que se desea cambiar dentro de la variable "linea"
+                nueva_linea=linea.replace(Cambio_palabra, Nueva_palabra)
+                Archivo_Nuevo.write(nueva_linea)
+            else:
+                Archivo_Nuevo.write(linea)
+        Archivo_Nuevo.close()
+        Archivo_original.close()
